52a3a9a2e782267f6330cd1f308174df82776000
Carlos Eduardo <duducp2013@gmail.com>
2018-06-17 23:31:06 -0300

    Configuações da bateria

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BatteryStatus } from '@ionic-native/battery-status';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,9 +10,33 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   statusBattery: number = 20;
+  isPlugged: Boolean;
+  subscription: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private batteryStatus: BatteryStatus,
+    public loadingCtrl: LoadingController
+  ) {
+  }
 
+  async ionViewDidLoad() {
+    const loader = this.loadingCtrl.create({
+      content: "Carregando dados do dispositivo..."
+    });
+    loader.present();
+
+    this.subscription = await this.batteryStatus.onChange().subscribe(status => {
+      this.statusBattery = status.level;
+      this.isPlugged = status.isPlugged;
+      console.log(status);
+    });
+
+    loader.dismiss();
+  }
+
+  ionViewDidLeave() {
+    this.subscription.unsubscribe();
   }
 
 }
